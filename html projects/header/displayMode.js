211c2c92af2c441eb12fe6651ca5613bdf8ca5b8
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2023-11-29 13:54:40 +0100

    reorganized and general development

--- html projects/header/displayMode.js
+++ html projects/header/displayMode.js
@@ -1,11 +1,8 @@
 
-export function toggleDisplayMode() {
-    alert('ran');
+function toggleDisplayMode() {
     if (document.body.classList.contains('darkmode')) {
         document.body.classList.remove('darkmode');
-        document.body.classList.add('lightmode');
     } else {
-        document.body.classList.remove('lightmode');
         document.body.classList.add('darkmode');
     }
 }
