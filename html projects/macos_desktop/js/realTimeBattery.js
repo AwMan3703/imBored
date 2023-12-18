de2c0474d1b166cacc62fea4c974fda0d6e4247a
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2023-12-18 13:59:51 +0100

    a fuck ton of stuff ion wanna write everything lol

---
+++ html projects/macos_desktop/js/realTimeBattery.js
@@ -0,0 +1,24 @@
+
+let batteryPc = 100;
+function updateBatteries() {
+    if (batteryPc == 1) {
+        batteryPc = 100;
+    }
+
+    const batteries = document.getElementsByClassName('real-time-battery');
+    const batteryStr = batteryPc + '% 🀰';
+    console.log(batteryPc+'% 🀰')
+
+    for (let i = 0; i < batteries.length; i++) {
+        const b = batteries[i];
+
+        b.innerText = batteryStr;
+    }
+
+    batteryPc -= 1;
+}
+
+updateBatteries();
+setInterval(function(){ 
+    updateBatteries();
+}, 10000);
