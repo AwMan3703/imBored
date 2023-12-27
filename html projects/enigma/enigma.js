288ca11c864312d10ab05588eaa301d3ebbfa2be
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2023-12-28 00:35:11 +0100

    working on enigma

--- html projects/enigma/enigma.js
+++ html projects/enigma/enigma.js
@@ -1,4 +1,19 @@
 
-const decrypt = () => {
-    
+const decryptables = document.getElementsByTagName('enigmajs-decrypt');
+
+let decrypt = (text, key) => {
+    let result = '';
+
+    /* This is where the decryption script goes */
+    result = '[decrypted]->' + text;
+
+    return result;
+}
+
+for (let i = 0; i < decryptables.length; i++) {
+    const e = decryptables[i];
+    const d = decrypt(e.innerText);
+
+    e.append(d);
+    e.remove();
 }
