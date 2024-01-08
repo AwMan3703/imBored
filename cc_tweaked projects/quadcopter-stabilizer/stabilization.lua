b7b02215c45d1e68374e6712d58a4d0d570c9822
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-01-08 17:59:28 +0100

    working on quad stabilizer

--- cc_tweaked projects/quadcopter-stabilizer/stabilization.lua
+++ cc_tweaked projects/quadcopter-stabilizer/stabilization.lua
@@ -15,6 +15,30 @@ local thruster_connections = {
     back_right = "back"
 }
 
+-- whatever you need to do to get roll value
+local function get_roll()
+    local roll = 0
+    return roll
+end
+
+-- whatever you need to do to get pitch value
+local function get_pitch()
+    local pitch = 0
+    return pitch
+end
+
+-- the position you want to correct to
+local target_position = {
+	roll = 0,
+	pitch = 0
+}
+
+-- the tilt threshold (in degrees) at which the thrusters are set to maximum power to correct the position
+local max_correction_threshold = {
+	roll = 45,
+	pitch = 45
+}
+
 
 -- CONSTANTS
 
@@ -24,14 +48,44 @@ local axes = {
     yaw = "AXES_YAW",
 }
 
+local signal_range_max = 30 --output signal range (0 to X)
+
+
+-- UTILITY
+
+-- scale a value between 0 and 1
+local function normalize(value, min, max) return (value - min) / (max - min) end
+
 
 -- SCRIPT
 
---in: axis, error; out: output signal position
---e.g. in: roll, -35; out: thruster_connections.back_right
-local function map_correction(axis, error)
-
+-- find how much signal you need to correct the tilt
+local function get_correction_signal(error)
+	if error == 0 then return {}, 0 end -- skip useless computing
+	local correction = 0 - error -- invert the error to find the correction
+	correction = normalize(correction, 0, 180) -- normalize to force between target_position and target_position + max_correction_threshold
+	correction = correction * signal_range_max -- range the correction
+	return correction
 end
 
+--in: axis, error; out: output_signals
+--	axis: the axis to map the correction on
+--	error: how much the tilt to correct is
+--	output_signals: data on how to throttle the thrusters to stabilize
+--	delta: the correction signal to output (in redtone intensity)
+--e.g.
+--		in: roll, -35
+--		out: {
+--			{thruster_connections.front_left, -3},
+--			{thruster_connections.front_right, +9},
+--			{thruster_connections.back_left, -3},
+--			{thruster_connections.back_right, +9}
+--		}
+local function get_mapped_correction(axis, error)
+	local correction = get_correction_signal(error)
+	--convert to output shown in comment
+end
+
+print(get_mapped_correction(axes.pitch, -67))
 
 
