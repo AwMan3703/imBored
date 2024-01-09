61163cc5c1122900a924229662526d264bd1c744
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-01-09 18:15:16 +0100

    working on quad stabilizer

--- cc_tweaked projects/quadcopter-stabilizer/stabilization.lua
+++ cc_tweaked projects/quadcopter-stabilizer/stabilization.lua
@@ -33,7 +33,7 @@ local max_correction_threshold = {
 
 -- CONSTANTS
 
-local signal_range_max = 30 --output signal range (0 to X)
+local signal_range_max = 15 --output signal range (0 to X)
 
 local axes = {
     roll = "AXES_ROLL",
@@ -83,11 +83,11 @@ end
 
 -- find how much signal you need to correct the tilt
 local function get_correction_signal(error)
-	if error == 0 then return {}, 0 end -- skip useless computing
-	local correction = 0 - error -- invert the error to find the correction
+	if error == 0 then return 0 end -- skip useless computing
+	local correction = error -- invert the error to find the correction
 	correction = normalize(correction, 0, 180) -- normalize to force between target_position and target_position + max_correction_threshold
 	correction = correction * signal_range_max -- range the correction
-	return correction
+	return 0 - correction
 end
 
 -- in: axis, error; out: output_signals
@@ -106,7 +106,7 @@ end
 local function get_mapped_correction(axis, error)
     local out = {}
 	local correction = get_correction_signal(error)
-    correction = math.abs(correction) --uhm sir redstone signals must be integers ☝️🤓
+    correction = math.ceil(correction) --uhm sir redstone signals must be integers ☝️🤓
 
 	-- Convert to output shown in comment --
     --get the affecting thrusters
@@ -123,14 +123,22 @@ end
 -- combine multiple get_mapped_correction() outputs
 local function sum_mapped_corrections(...)
     local out = {
-        thruster_connections.front_left,
-        thruster_connections.front_right,
-        thruster_connections.back_left,
-        thruster_connections.back_right
+        [thruster_connections.front_left] = 0,
+        [thruster_connections.front_right] = 0,
+        [thruster_connections.back_left] = 0,
+        [thruster_connections.back_right] = 0
     }
 
     for i=1, #arg do
-        print(i)
+        local mapped_correction = arg[i]
+
+        for i=1, #mapped_correction do
+            local correction = mapped_correction[i]
+            local target = correction[1]
+            local variation = correction[2]
+
+            out[target] = out[target] + variation
+        end
     end
 
     return out
@@ -138,19 +146,26 @@ end
 
 -- basically just set engines to the corrected power level
 local function apply_corrections(summed_corrections)
-    
+    print(
+        textutils.serialize(
+            summed_corrections
+        )
+    )
+    for thruster, value in pairs(summed_corrections) do
+        print("set thruster "..thruster.." to "..value)
+        redstone.setAnalogOutput(thruster, value)
+    end
 end
 
 
 
 local function main()
 
-    print(textutils.serialize(
+    apply_corrections(
         sum_mapped_corrections(
             get_mapped_correction(axes.roll, -67),
-            get_mapped_correction(axes.pitch, -42)
+            get_mapped_correction(axes.pitch, 42)
         )
-    ))
-    sum_mapped_corrections("cock", "blass")
+    )
 
 end main()
