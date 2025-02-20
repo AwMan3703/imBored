eddab80a2b3767e338823eebdf49dee632cfe1e4
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2025-02-20 18:34:48 +0100

    made index a bit better

--- html projects/_index/main.js
+++ html projects/_index/main.js
@@ -1,17 +1,17 @@
-
+const incorporateOnHoverToggle = document.getElementById('incorporate-on-hover-toggle')
 
 //run on load
 function setup(listOutput, projectdataElements) {
     for (data of projectdataElements) {
         const li = document.createElement("li");
-        li.innerHTML = `<h3>${data.innerText}</h3><a href=\"${data.getAttribute("path")}\">open</a> or <button onclick=\"incorporate(\'${data.getAttribute("path")}\')\">incorporate</button>`;
-        // Uncomment line 9 to automatically incorporate projects when menu item is hovered
-        //li.setAttribute("onmouseenter", `incorporate(\"${data.getAttribute("path")}\");`)
+        li.innerHTML = `<h3>${data.innerText}</h3><a href=\"${data.getAttribute("path")}\">open</a> or <button onclick=\"incorporate(\'${data.getAttribute("path")}\', true)\">incorporate</button>`;
+        li.setAttribute("onmouseenter", `incorporate(\"${data.getAttribute("path")}\", false);`)
         listOutput.appendChild(li);
     }
 }
 
-function incorporate(path) {
+function incorporate(path, clicked) {
+    if (!(incorporateOnHoverToggle.checked || clicked)) return
     const prevbox = document.getElementById('preview-box')
     prevbox.innerHTML = '';
     const preview = document.createElement('iframe');
@@ -20,3 +20,5 @@ function incorporate(path) {
     preview.style.height = 'inherit';
     prevbox.appendChild(preview);
 }
+
+incorporateOnHoverToggle.checked = false
